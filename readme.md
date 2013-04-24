0df5ed2b52d5305b11025e0355d1a2123d10e5ba
Caleb James DeLisle <calebdelisle@lavabit.com>
2013-04-24 11:21:26 -0400

    Use new version of xwiki-tools which can generate a maven project.

--- readme.md
+++ readme.md
@@ -10,3 +10,12 @@ To build it manually first check that node.js is installed and install `xwiki-to
     npm install -g xwiki-tools
 
 Then type `./do`.
+
+To install the files directly to an XWiki installation, use:
+
+    ## The format is --post <username>:<password>@<hostname>[:<port>]/<preview path>/
+    ./do --post Admin:admin@192.168.1.1:8080/xwiki/bin/preview//
+
+To generate a maven build:
+
+    ./do --mvn
--- src/make.js
+++ src/make.js
@@ -15,22 +15,22 @@ doc.addAttachment("src/js/external/jio.js");
 doc.addAttachment("src/js/external/md5.js");
 
 
+var pack = new XWiki.Package();
+pack.setName("XWiki - Contrib - Resilience");
+pack.setDescription("A container for platform independent web gadgets");
+pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-resilience");
+pack.addDocument(doc);
+
 // Post to a wiki?
 // must post to a /preview/ page, for example:
 // syntax  ./do --post Admin:admin@192.168.1.1:8080/xwiki/bin/preview//
-var i = process.argv.indexOf('--post');
-if (i > -1) {
-    var userUrl = process.argv[i+1].split('@').reverse();
-    XWiki.Tools.postDocToWiki(userUrl[1], userUrl[0], doc, function() {
-        console.log("done");
-    });
+var i;
+if ((i = process.argv.indexOf('--post')) > -1) {
+    pack.postToWiki(process.argv[i+1]);
+
+} else if ((i = process.argv.indexOf('--mvn')) > -1) {
+    pack.genMvn('mvnout');
 
 } else {
-    var pack = new XWiki.Package();
-    pack.setName("XWiki - Contrib - Resilience");
-    pack.setDescription("A container for platform independent web gadgets");
-    pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-resilience");
-    pack.addDocument(doc);
-    pack.beginZip('XWiki.Resilience.xar');
-    pack.finalize();
+    pack.genXar('XWiki.Resilience.xar');
 }
